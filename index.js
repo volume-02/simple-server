5e1fa0e6aad0206bd1548365947e1bd14c9cb572
volume02 <borsukboris@gmail.com>
2023-10-05 12:44:27 +0700

    get & post

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/:url', (req, res) => {
+    console.log(req.params.url)
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    let data = req.body;
+    console.log(data);
+    res.send('Data Received: ' + JSON.stringify(data));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
