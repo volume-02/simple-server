e13aece02ed1f0f4f4bfc2e146a07261ace7e335
volume-02 <borsukboris@gmail.com>
2023-11-06 11:28:10 +0700

    prisma

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 const port = 3000
@@ -13,10 +14,45 @@ app.get('/:url', (req, res) => {
 
 app.post('/', (req, res) => {
     const data = req.body;
-    console.log(data + "qweqwe");
+  console.log(data);
     res.send('Data Received: ' + JSON.stringify(data));
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+   await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
